95a78658b5d8f6506857f37cd25c6ee52abd43af
Auston <100736253+AuC1125@users.noreply.github.com>
2024-08-31 04:33:09 +1000

    GDT1 movements updates

---
+++ GDT1 Project/Assets/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 10f;
+
+    //private Vector3 direction = new Vector3(0f,0f,1f);
+    private PlayerActions actions;
+    private InputAction movementAction;
+
+    void Awake()
+    {
+        actions = new PlayerActions();
+        movementAction = actions.walking.movement;
+    }
+
+    void OnEnable()
+    {
+        movementAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        movementAction.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = movementAction.ReadValue<Vector2>();
+        Vector3 direction = new Vector3(move.x, move.y, 0f);
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
